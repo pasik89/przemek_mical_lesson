a16175cf9515a2fc26bd3b2c20e9337e8dcbf704
Przemek Lisowski <przemek_lisowski@o2.pl>
2023-05-09 22:29:05 +0200

    generate section blocks from server

--- js/httpGet.js
+++ js/httpGet.js
@@ -1,19 +1,19 @@
 export function httpGet(endpoint) {
     return new Promise(function (resolve, reject) {
-      const xhr = new XMLHttpRequest();
-  
-      xhr.open('GET', `http://localhost:3000/${endpoint}`);
-  
-      xhr.onload = function () {
-          const status = xhr.status;
-  
-          if (status == 200) {
-              resolve(JSON.parse(xhr.response));
-          } else {
-              reject(status);
-          }
-      };
-  
-      xhr.send();
+        const xhr = new XMLHttpRequest();
+
+        xhr.open('GET', `http://localhost:3000/${endpoint}`);
+
+        xhr.onload = function () {
+            const status = xhr.status;
+
+            if (status == 200) {
+                resolve(JSON.parse(xhr.response));
+            } else {
+                reject(status);
+            }
+        };
+
+        xhr.send();
     });
-  }
+}
--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import "../css/main.css";
 import "../scss/style.scss";
 import "../img/index";
 import { httpGet } from "./httpGet"
+// import { httpGetBlocks } from "./httpGet"
 
 class Header extends HTMLElement {
   constructor() {
@@ -12,10 +13,10 @@ class Header extends HTMLElement {
     this.innerHTML = `
       <nav>
       <a><img src="img/logo.png" alt="logo"></a>
-          <a>Home </a>
-          <a>About </a>
-          <a>Furnitures </a>
-          <a>Testimonial </a>
+          <a>Home</a>
+          <a>About</a>
+          <a>Furnitures</a>
+          <a>Testimonial</a>
           <a>Contact Us</a>
           <a><i class="fa-regular fa-user"></i>Login/Register</a>
           <a><i class="fa-solid fa-magnifying-glass"></i></a>
@@ -42,4 +43,63 @@ menuElements.forEach(element => {
   menuListElement.appendChild(menuListItem);
   menuListItem.classList.add('navi__list-element');
   menuListItem.innerHTML = menuItemHtml;
+});
+
+// blocks section
+
+// const blocksContainer = document.querySelector('.blocks__container');
+// const block = document.createElement('div');
+
+// blocksContainer.appendChild(block);
+// block.classList.add('blocks__block');
+
+// const blocksElements = await httpGet('carousel');
+
+// blocksElements.forEach(element => {
+//   const blocksItemHtmlTitle = `<h3 class="blocks__title">${element.title}</h3>`;
+//   const blocksItemHtmlImg = `<img class="blocks__image" src="${element.ImgName}">`;
+//   const blocksItemHtmlContent = `<p class="blocks__content">${element.description}</p>`;
+//   const blocksListElement = document.querySelector('.blocks__block');
+//   const blocksItemTitle = document.createElement('h3');
+//   const blocksItemImg = document.createElement('img');
+//   const blocksItemContent = document.createElement('p');
+
+//   blocksListElement.appendChild(blocksItemTitle);
+//   blocksListElement.appendChild(blocksItemImg);
+//   blocksListElement.appendChild(blocksItemContent);
+//   blocksItemTitle.classList.add('blocks__title');
+//   blocksItemImg.classList.add('blocks__image');
+//   blocksItemContent.classList.add('blocks__content');
+//   blocksItemTitle.innerHTML = blocksItemHtmlTitle;
+//   blocksItemImg.innerHTML = blocksItemHtmlImg;
+//   blocksItemContent.innerHTML = blocksItemHtmlContent;
+// });
+
+const blocksContainer = document.querySelector('.blocks__container');
+
+const blocksElements = await httpGet('carousel');
+
+blocksElements.forEach(element => {
+
+  const block = document.createElement('div');
+  blocksContainer.appendChild(block);
+  block.classList.add('blocks__block');
+
+  const blocksItemHtmlTitle = `<h3 class="blocks__title">${element.title}</h3>`;
+  const blocksItemHtmlImg = `<img class="blocks__image" src="${element.ImgName}">`;
+  const blocksItemHtmlContent = `<p class="blocks__content">${element.description}</p>`;
+  const blocksListElement = document.querySelector('.blocks__block');
+  const blocksItemTitle = document.createElement('h3');
+  const blocksItemImg = document.createElement('img');
+  const blocksItemContent = document.createElement('p');
+
+  blocksListElement.appendChild(blocksItemTitle);
+  blocksListElement.appendChild(blocksItemImg);
+  blocksListElement.appendChild(blocksItemContent);
+  blocksItemTitle.classList.add('blocks__title');
+  blocksItemImg.classList.add('blocks__image');
+  blocksItemContent.classList.add('blocks__content');
+  blocksItemTitle.innerHTML = blocksItemHtmlTitle;
+  blocksItemImg.innerHTML = blocksItemHtmlImg;
+  blocksItemContent.innerHTML = blocksItemHtmlContent;
 });
